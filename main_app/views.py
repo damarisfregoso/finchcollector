aee1b32dba2cbdcb88213efa0a2e1e9487c79b5a
damarisfreg <fregosodmar24@gmail.com>
2023-09-22 15:27:10 -0700

    About and Index working

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Scary', 'breed': 'House', 'description': 'feathery little demon', 'age': 3},
+  {'name': 'Ugg', 'breed': 'Purple', 'description': 'weird hair', 'age': 2},
+  {'name': 'Ewy', 'breed': 'Dessert', 'description': 'very dull color', 'age': 5},
+  {'name': 'Chicky', 'breed': 'Atlantic', 'description': 'yellow like a chick', 'age': 0},
+]
+
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finch_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
